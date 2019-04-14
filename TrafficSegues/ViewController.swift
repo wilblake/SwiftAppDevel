9d56592b5d7bbbde60049401c76c451cf838f0f2
Wil B <wblake@emsys.net>
2019-04-14 14:13:35 -0400

    TrafficSegue

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func unwindToRed (unwindSegue: UIStoryboardSegue)
+    {
 }
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            segue.destination.navigationItem.title = textField.text
+    }
+}
