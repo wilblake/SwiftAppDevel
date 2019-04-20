d441e466fae2edc198fae3321844a85025a590dd
Wil B <wblake@emsys.net>
2019-04-20 17:10:37 -0400

    Quick and Dirty

--- Accelerometer/ViewController.swift
+++ Accelerometer/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+// import CoreMotion
+
+
+// var motionManager:CMMotionManager!
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+     /*   motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates() */
+        
     }
 
-
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        print("shaken, not stirred")
+   /*    let accelerometerData = motionManager.accelerometerData
+    
+        let sX = NSString(format: "%2f", accelerometerData!.acceleration.x)
+        let sY = NSString(format: "%.2f", accelerometerData!.acceleration.y)
+        
+        print("Acceleration x \(sX) y \(sY)") */
+    }
+    
 }
 
